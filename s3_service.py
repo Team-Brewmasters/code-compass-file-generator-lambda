b2b752d608777939decedaaa4cdeaf54259a86d9
John Aquino <johnaquino@outlook.com>
2024-04-30 13:28:42 -0400

    Fix method signatures in S3Service

--- s3_service.py
+++ s3_service.py
@@ -4,10 +4,10 @@ s3 = boto3.client('s3')
 
 class S3Service:
 
-    def upload_file_to_s3(bucket, key, data):
+    def upload_file_to_s3(self, bucket, key, data):
             s3.put_object(Bucket=bucket, Key=key, Body=data)
 
-    def generate_presigned_url(bucket, key, expiration=3600):
+    def generate_presigned_url(self, bucket, key, expiration=3600):
         return s3.generate_presigned_url('get_object',
                                       Params={'Bucket': bucket, 'Key': key},
                                       ExpiresIn=expiration)
